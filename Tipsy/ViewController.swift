9503b0da855f7f184ca3f64720e59ccce625c731
calebripley <calebaripley@gmail.com>
2015-12-16 19:06:31 -0700

    Second commit. Added text field as first responder, animations, colors, and re-did UI.

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipsyButtonLabel: UILabel!
+    @IBOutlet weak var tipsySwitch: UISwitch!
+    @IBOutlet weak var billFieldVerticalSpaceConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tipControlVerticalSpaceConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tipsyButtonLabelVerticalSpaceConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tipsySwitchVerticalSpaceConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tipLabelVerticalSpaceConstraint: NSLayoutConstraint!
+    @IBOutlet weak var totalLabelVerticalSpaceConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +31,22 @@ class ViewController: UIViewController {
         
         let defaults = NSUserDefaults.standardUserDefaults()
         tipControl.selectedSegmentIndex = defaults.integerForKey("defaultTipPercentage")
+        
+        billField.becomeFirstResponder()
+        
+        totalLabelVerticalSpaceConstraint.constant -= self.view.bounds.height
+        
+        tipLabelVerticalSpaceConstraint.constant -= self.view.bounds.height
+        
+        tipsySwitchVerticalSpaceConstraint.constant -= self.view.bounds.height
+    
+        tipsyButtonLabelVerticalSpaceConstraint.constant -= self.view.bounds.height
+        
+        tipControlVerticalSpaceConstraint.constant -= self.view.bounds.height
+        
+        self.billFieldVerticalSpaceConstraint.constant = 350
+        
+        navigationController!.navigationBar.barTintColor = UIColor.orangeColor()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -37,17 +62,105 @@ class ViewController: UIViewController {
 
     @IBAction func onEditingChanged(sender: AnyObject) {
         var tipPercentages = [0.1, 0.15, 0.2]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         var billAmount = NSString(string: billField.text!).doubleValue
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
+        if billField.text == "" {
+            totalLabelVerticalSpaceConstraint.constant -= self.view.bounds.height
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.totalLabel.layoutIfNeeded()
+                }, completion: nil)
+            
+            tipLabelVerticalSpaceConstraint.constant -= self.view.bounds.height
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.tipLabel.layoutIfNeeded()
+                }, completion: nil)
+            
+            tipsySwitchVerticalSpaceConstraint.constant -= self.view.bounds.height
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.tipsySwitch.layoutIfNeeded()
+                }, completion: nil)
+            
+            tipsyButtonLabelVerticalSpaceConstraint.constant -= self.view.bounds.height
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.tipsyButtonLabel.layoutIfNeeded()
+                }, completion: nil)
+            
+            tipControlVerticalSpaceConstraint.constant -= self.view.bounds.height
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.tipControl.layoutIfNeeded()
+                }, completion: nil)
+            
+            self.billFieldVerticalSpaceConstraint.constant = 350
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.billField.layoutIfNeeded()
+                }, completion: nil)
+        } else {
+            
+            totalLabelVerticalSpaceConstraint.constant = 237.5
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.totalLabel.layoutIfNeeded()
+                }, completion: nil)
+            
+            tipLabelVerticalSpaceConstraint.constant = 300
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.tipLabel.layoutIfNeeded()
+                }, completion: nil)
+            
+            tipsySwitchVerticalSpaceConstraint.constant = 375
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.tipsySwitch.layoutIfNeeded()
+                }, completion: nil)
+            
+            tipsyButtonLabelVerticalSpaceConstraint.constant = 375
+            UIView.animateWithDuration(0.3, delay: 1.0, options: [], animations: { self.tipsyButtonLabel.layoutIfNeeded()
+                }, completion: nil)
+            
+            tipControlVerticalSpaceConstraint.constant = 450
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: { self.tipControl.layoutIfNeeded()
+                }, completion: nil)
+            
+            self.billFieldVerticalSpaceConstraint.constant = 500
+            UIView.animateWithDuration(0.3, delay: 0.0, options: [], animations: {
+                self.billField.layoutIfNeeded()
+                }, completion: nil)
+        }
+        
+        if tipsySwitch.on == true {
+            billAmount = NSString(string: billField.text!).doubleValue
+            tipPercentage = 0.3
+            tip = billAmount * tipPercentage
+            total = billAmount + tip
+            
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+        } else {
+            tipPercentages = [0.1, 0.15, 0.2]
+            billAmount = NSString(string: billField.text!).doubleValue
+            tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+            tip = billAmount * tipPercentage
+            total = billAmount + tip
+            
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+        }
     }
-
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
+   
+    @IBAction func feelinTipsy(sender: UISwitch) {
+        if tipsySwitch.on == true {
+            var billAmount = NSString(string: billField.text!).doubleValue
+            var tipPercentage = 0.3
+            var tip = billAmount * tipPercentage
+            var total = billAmount + tip
+            
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+        } else {
+            var tipPercentages = [0.1, 0.15, 0.2]
+            var billAmount = NSString(string: billField.text!).doubleValue
+            var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+            var tip = billAmount * tipPercentage
+            var total = billAmount + tip
+            
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+        }
     }
 }
 
